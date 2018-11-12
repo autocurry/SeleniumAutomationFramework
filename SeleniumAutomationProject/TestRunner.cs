d038ecdcf75e18c899bd7421b462ee897fa84db5
sandeep <sandeepsekhar555@gmail.com>
2018-11-12 22:43:18 +0800

    first commit

---
+++ SeleniumAutomationProject/TestRunner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumAutomationProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
