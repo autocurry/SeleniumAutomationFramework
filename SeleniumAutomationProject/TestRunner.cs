fd579dbd0b399d4a261912828148e280511c8a1d
sandeep <sandeepsekhar555@gmail.com>
2018-11-12 23:39:35 +0800

    added the selenium test

--- SeleniumAutomationProject/TestRunner.cs
+++ SeleniumAutomationProject/TestRunner.cs
@@ -1,17 +1,30 @@
 using Xunit;
 using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 public class TestRunner : IDisposable
 {
+    IWebDriver _driver;
     public TestRunner ()
     {
         Console.WriteLine("inside the setup");
+        _driver= new ChromeDriver("/home/sandeep/Desktop/SeleniumAutomation/SeleniumAutomationFramework/SeleniumAutomationProject/Driver");
        
     }
 
     public void Dispose()
     {
         Console.Write("inside the teardown");
+    }
+
+    [Fact]
+    public void FirstTest()
+    {
+    
+    Console.WriteLine("inside the first test");
+    _driver.Navigate().GoToUrl("https://www.automationpractice.com");
+    
     }
 }
 
