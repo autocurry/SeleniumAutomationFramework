22cfda8b4449ecc2146e628ece033baaa9c9d4ef
sandeep <sandeepsekhar555@gmail.com>
2018-11-26 22:59:48 +0800

    added the waits for checkout

--- Pages/CheckOutPage.cs
+++ Pages/CheckOutPage.cs
@@ -7,5 +7,16 @@ public class CheckOutPage{
 By DisplayedItemPrice = By.CssSelector(".pricedisplay");
 By ItemMagicMouse = By.CssSelector("a[href*='magic-mouse']");
 
+//Verfies the price is same for the item displayed while adding to cart and price during checkout
+public bool VerifyItemPrice(string expectedprice)
+{
+    var result = false;
+    var displayedprice = Driver._driver.FindElement(DisplayedItemPrice).Text;
+    if(expectedprice.Equals(displayedprice))
+    {
+        result=true;
+    }
+    return result;
+}
 
 }
