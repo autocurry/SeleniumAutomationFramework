a4e63a8522e7f35b177e5cd18a4383ad8832b236
sandeep <sandeepsekhar555@gmail.com>
2018-11-26 22:54:01 +0800

    created magic mouse checkout pages

---
+++ Pages/CheckOutPage.cs
@@ -0,0 +1,11 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using System.Collections.Generic;
+
+public class CheckOutPage{
+
+By DisplayedItemPrice = By.CssSelector(".pricedisplay");
+By ItemMagicMouse = By.CssSelector("a[href*='magic-mouse']");
+
+
+}
--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -7,7 +7,13 @@ public class HomePage{
   MyAccountPage _myAccountPage ;
     By MyAccountLink = By.CssSelector("a[title='My Account']");
     By HomeMenu = By.LinkText("Home");
-  
+    By SearchBox = By.CssSelector("input[value='Search Products']");
+    By CartItemCount = By.CssSelector(".count");
+    By ItemMagicMouse = By.CssSelector("a[href*='magic-mouse']");
+    By CurrentPrice = By.CssSelector(".currentprice");
+    By AddToCartButton = By.CssSelector("input[value='Add To Cart']");
+    By LoadingImage = By.CssSelector("img[alt='loading']");
+
 
 
     public MyAccountPage OpenMyAccount()
