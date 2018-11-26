d2a39cf5f33b3bac5601967319321c0ae63d9db0
sandeep <sandeepsekhar555@gmail.com>
2018-11-26 21:33:11 +0800

    home page test case completed

--- Pages/HomePage.cs
+++ Pages/HomePage.cs
@@ -1,16 +1,26 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support;
+using System.Collections.Generic;
 
 public class HomePage{
 
   MyAccountPage _myAccountPage ;
     By MyAccountLink = By.CssSelector("a[title='My Account']");
+    By HomeMenu = By.LinkText("Home");
+  
+
 
     public MyAccountPage OpenMyAccount()
     {
         Driver._driver.FindElement(MyAccountLink).Click();
         _myAccountPage = new MyAccountPage();
         return _myAccountPage;
+    }
+
+    public void Verify()
+    {        
+        Driver._driver.FindElement(HomeMenu).Click();
+       
     }
 
 }
--- Tests/HomePageShould.cs
+++ Tests/HomePageShould.cs
@@ -2,19 +2,28 @@ using OpenQA.Selenium;
 using  OpenQA.Selenium.Chrome;
 using Xunit;
 using System.IO;
-public class HomePageShould{
+using System;
+public class HomePageShould:BasePage,IDisposable{
 
+HomePage _homePage;
 IWebDriver _driver ;
+
+public HomePageShould():base()
+    {
+        
+
+    } 
 
 [Fact]
 public void LoadCorrectly()
 {
-    var path = Directory.GetCurrentDirectory();
-     _driver = new ChromeDriver(path+"/Driver/");
-     _driver.Navigate().GoToUrl("https://www.google.com.sg/");
-     _driver.Quit();
+    _homePage = new HomePage();
+     _homePage.Verify();
 }
 
-
+public void Dispose()
+    {
+        Driver._driver.Quit();
+    }
 
 }
